ad66e626677fdffaa3484504779ea033e635566c
endemitko <endemitko@gmail.com>
2013-06-28 18:32:41 +0200

    first class CSS

--- www/index.php
+++ www/index.php
@@ -18,11 +18,17 @@ $versions = $files->getVersions();
 		<meta charset="utf-8" />
 		<meta name="author" content="Michal Hojgr & Patrik Beneš" />
 		<title>Čarovný minecraft</title>
+        <link rel="stylesheet" href="style.css">
 	</head>
 	<body>
+    <div class="header">
+        <div class="logo"> </div>
+        <h4>Posledni verze: <?php echo $versions["latest"]; ?></h4>  <a href=""><div class="button">DOWNLOAD</div> </a>
+    </div>
+
 		<h1>Carovny minecraft</h1>
 
-		<h2>Posledni verze: <?php echo $versions["latest"]; ?></h2>
+
 
 		<h3>Dalsi verze</h3>
 
