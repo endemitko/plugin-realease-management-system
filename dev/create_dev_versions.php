52b518de850e988e2c83ed4d428ad9f60657daa4
endemitko <endemitko@gmail.com>
2013-06-28 16:22:20 +0200

    Tweak test version generator

--- dev/create_dev_versions.php
+++ dev/create_dev_versions.php
@@ -1,6 +1,10 @@
 <?php
+
+
 function make_dev_versions() {
-	file_put_contents("../versions/versions-test-" . rand(0, 3) . "." . rand(0, 3) . "." . rand(0, 3) . ".txt", "");
+    $name =  "../versions/versions-test-" . rand(0, 3) . "." . rand(0, 3) . "." . rand(0, 3) . ".txt";
+	file_put_contents($name, "");
+	echo $name . "\n";
 }
 
 for($i = 0; $i < 10; $i ++) {
